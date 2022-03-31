e4a642d4f8177005b315bedd721ddc6d3c92ee0f
Sarah Moffat <sarah.moffat5@gmail.com>
2022-03-31 11:19:35 +0100

    Small changes made

--- lib/account.rb
+++ lib/account.rb
@@ -11,16 +11,13 @@ class Account
 
   def deposit(money_in, date = TODAYS_DATE)
     @balance += money_in
-    puts "Deposited: £#{money_in}. Updated balance: £#{@balance}."
-    @transactions << "#{date} || #{'%.2f' % money_in} || #{@money_out}|| #{format('%.2f', 
-@balance)}"
+    @transactions << "#{date} || #{'%.2f' % money_in} || #{@money_out}|| #{format('%.2f', @balance)}"
   end
 
   def withdrawal(money_out, date = TODAYS_DATE)
     fail 'Insufficient funds for withdrawal' if @balance < money_out
     @money_out = money_out
     @balance -= money_out
-    puts "Withdrew: £#{money_out}. Updated balance: £#{@balance}."
     @transactions << "#{date} || || #{format('%.2f', @money_out)} || #{format('%.2f', @balance)}"
   end
 
